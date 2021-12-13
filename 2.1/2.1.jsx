c8ddf86d196703ffd98b719f0a6ab4d62ffc2b85
galpalex <galpalex@gmail.com>
2021-12-13 11:01:11 +0200

    2.1 react init

---
+++ 2.1/2.1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+document.title = "AppleSeeds React App";
+const Hello = () => {
+  return (
+    <div>
+      <a href="/">Click Me</a>
+    </div>
+  );
+};
+
+export default Hello;
