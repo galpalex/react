d962f0ace4490c44c342c9c3d1719fd216afef13
galpalex <galpalex@gmail.com>
2021-12-13 18:41:54 +0200

    4.1 react init

---
+++ 4.1/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./4.1";
+
+const Main = () => {
+  return (
+    <div class="container">
+      <Button text="Important" font="bold" />
+      <Button text="Not Important" />
+    </div>
+  );
+};
+
+export default Main;
